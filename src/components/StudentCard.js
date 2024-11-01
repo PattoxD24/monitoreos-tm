26a75a5e252ef5b9134e7f72634856110bd7d510
Patto xD <edgaripatricio@gmail.com>
2024-10-31 21:13:04 -0600

    Update StudentCard.js

--- src/components/StudentCard.js
+++ src/components/StudentCard.js
@@ -7,7 +7,7 @@ export default function StudentCard({ student, onClick, onDelete }) {
       className="relative cursor-pointer p-4 border rounded-lg shadow hover:shadow-lg transition"
       style={{ backgroundColor: student.backgroundColor }} // Aplicar el color de fondo
     >
-      <h2 className="text-lg font-bold text-grey-600">{student.matricula}</h2> 
+      <h2 className="text-lg font-bold text-gray-600">{student.matricula}</h2> 
       <p className="text-sm font-semibold text-blue-600">{student.preferredName}</p>
       <p className="text-sm text-gray-700">{student.fullName}</p>
       
