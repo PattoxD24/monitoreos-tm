1cc0aa899ed50c0184b3a6468e0a88e236cc9753
Edgar Patricio <Edgar@foodsafetycts.com>
2024-11-02 18:54:37 -0600

    Enhance FileUploader component with labels for file inputs and improved user feedback

--- src/components/FileUploader.js
+++ src/components/FileUploader.js
@@ -7,12 +7,31 @@ export default function FileUploader({ onFile1Change, onFile2Change, onProcessFi
         type="file"
         accept=".xlsx, .xls"
         onChange={onFile1Change}
+        placeholder="Base"
+        id="file1"
+        className="hidden"
       />
+      <label
+        htmlFor="file1"
+        className="rounded-full border border-solid border-transparent transition-colors bg-foreground text-background text-sm h-10 px-4 flex items-center justify-center"
+      >
+        {document.getElementById('file1')?.files[0]?.name || 'Selecciona archivo Base'}
+        
+      </label>
       <input
         type="file"
         accept=".xlsx, .xls"
         onChange={onFile2Change}
+        placeholder="Monitoreos"
+        id="file2"
+        className="hidden"
       />
+      <label
+        htmlFor="file2"
+        className="rounded-full border border-solid border-transparent transition-colors bg-foreground text-background text-sm h-10 px-4 flex items-center justify-center"
+      >
+        {document.getElementById('file2')?.files[0]?.name || 'Selecciona archivo Monitoreos'}
+      </label>
       <button
         onClick={onProcessFiles}
         className="rounded-full border border-solid border-transparent transition-colors bg-foreground text-background text-sm h-10 px-4"
